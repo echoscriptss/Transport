545836431fdc55899a2a8c982b2944b83efeb619
prabhjootsingh <prabhjoot.singh@programming.com>
2026-01-13 14:06:37 +0530

    working on side menu

--- Despir/View/Home/HomeView.swift
+++ Despir/View/Home/HomeView.swift
@@ -9,33 +9,38 @@ import SwiftUI
 
 struct HomeView: View {
   @EnvironmentObject var appRootManager: AppRootManager
-
+  @State private var isMenuOpen = false
+  
     var body: some View {
-      VStack {
-        Text("Home Screen")
-          .font(.system(size: 36.0).bold())
-        Spacer()
-        Text("Long press for settings")
-          .font(.system(size: 20.0).bold())
-          .contextMenu {
-            Button("Update Profile") {
-              appRootManager.push(.profile)
+      
+     
+        VStack {
+          Spacer().frame(height: 50)
+          Text("Home Screen")
+            .font(.system(size: 36.0).bold())
+          Spacer()
+          Text("Long press for settings")
+            .font(.system(size: 20.0).bold())
+            .contextMenu {
+              Button("Update Profile") {
+                appRootManager.push(.profile)
+              }
+              Button("Change Password") {
+                appRootManager.push(.changePassword)
+              }
             }
-            Button("Change Password") {
-              appRootManager.push(.changePassword)
-            }
-          }
-        Spacer()
-        Button {
-          DataManager.isUserLoggedIn = false
-          appRootManager.currentRoot = .login
-          
-        } label: {
-          Text("Logout")
+          Spacer()
+          Button {
+            DataManager.isUserLoggedIn = false
+            appRootManager.currentRoot = .login
             
+          } label: {
+            Text("Logout")
+            
+          }
+          Spacer()
         }
-        Spacer()
-      }
+      
       .navigationDestination(for: Route.self) { route in
           if route == .changePassword {
               ChangePasswordView()
@@ -43,7 +48,10 @@ struct HomeView: View {
             ProfileView()
           }
       }
+
     }
+  
+ 
 }
 
 #Preview {
